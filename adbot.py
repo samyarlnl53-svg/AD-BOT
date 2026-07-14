8efc819be774814644340947e4b6260108f21cec
samyarlnl53-svg <samyarlnl53@gmail.com>
2026-07-14 22:51:50 +0330

    Update adbot.py

--- adbot.py
+++ adbot.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 # تنظیمات پیش‌فرض
 CONFIG_FILE = "bot_config.json"
 DEFAULT_CONFIG = {
-    "host_usernames": ["ad0ri"],
+    "host_usernames": ["samyardj"],
     "admin_usernames": ["ad0ri"],
     "vip_usernames": [],
     "banned_users": [],
@@ -2713,8 +2713,8 @@ async def main():
     from http.server import BaseHTTPRequestHandler, HTTPServer
     
     logger.info("تلاش برای بارگذاری متغیرهای محیطی...")
-    room_id = os.getenv("ROOM_ID", "68e771922d585712212e8070")
-    api_token = os.getenv("API_TOKEN", "ed578ca3bb6a6592059a878e4d807d0c7eac56742239fdcce7ca84841f2437e5")
+    room_id = os.getenv("ROOM_ID", "68a4105469081601c668f053")
+    api_token = os.getenv("API_TOKEN", "fa5e8db82341930610b20d38fbb912628d664739c1653583697447f6651ae2d7")
     
     if not room_id or not api_token:
         logger.error("ROOM_ID یا API_TOKEN تنظیم نشده‌اند.")
